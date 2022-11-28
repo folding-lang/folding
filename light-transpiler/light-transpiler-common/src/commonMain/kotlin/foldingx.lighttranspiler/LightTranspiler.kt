0fad68151ea15388b97fb69683cf0dd293f2a475
muqhc <muqhc07@gmail.com>
2022-11-28 22:07:36 +0900

    improve import syntax

--- light-transpiler/light-transpiler-common/src/commonMain/kotlin/foldingx.lighttranspiler/LightTranspiler.kt
+++ light-transpiler/light-transpiler-common/src/commonMain/kotlin/foldingx.lighttranspiler/LightTranspiler.kt
@@ -4,6 +4,7 @@ import foldingx.parser.FoldingParser
 
 interface LightTranspiler {
     fun transpile(fdFileContext: FoldingParser.FileContext): String
+    fun processImportEx(fdImportExContext: FoldingParser.ImportExContext): String
     fun processFileCompo(fdFileCompoContext: FoldingParser.FileCompoContext): String
     fun processDefinition(fdDefinitionContext: FoldingParser.DefinitionContext): String
     fun processDef(fdDefContext: FoldingParser.DefContext): String
