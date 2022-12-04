529031082c4c8f5c93166354b19cad809899c143
muqhc <muqhc07@gmail.com>
2022-12-04 19:37:06 +0900

    the char code translate to hex

--- folding-parser/src/commonMain/kotlin/foldingx/parser/identifier/processId.kt
+++ folding-parser/src/commonMain/kotlin/foldingx/parser/identifier/processId.kt
@@ -5,12 +5,12 @@ import foldingx.parser.FoldingParser
 fun processId(idSource: FoldingParser.CommonIdentifierContext) = when {
     idSource.ID() != null -> idSource.text
     idSource.findOpIdWrap() != null ->
-        "OP_" + idSource.findOpIdWrap()!!.OPID()!!.text.toCharArray().joinToString("") {
-            it.code.toString()
+        "OP_" + idSource.findOpIdWrap()!!.OPID()!!.text.toCharArray().joinToString("_") {
+            it.code.toString(16)
         }
     idSource.findAopIdWrap() != null ->
-        "AOP_" + idSource.findAopIdWrap()!!.OPID()!!.text.toCharArray().joinToString("") {
-            it.code.toString()
+        "AOP_" + idSource.findAopIdWrap()!!.OPID()!!.text.toCharArray().joinToString("_") {
+            it.code.toString(16)
         }
     else -> throw RuntimeException("'${idSource.text}' is invalid Id")
 }
