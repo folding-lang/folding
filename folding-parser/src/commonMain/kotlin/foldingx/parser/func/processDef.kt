b137ffd50dc79ac2e04f0911a90d047e5800ccda
muqhc <muqhc07@gmail.com>
2022-12-05 19:53:45 +0900

    impl process of justDef and foreignDef

--- folding-parser/src/commonMain/kotlin/foldingx/parser/func/processDef.kt
+++ folding-parser/src/commonMain/kotlin/foldingx/parser/func/processDef.kt
@@ -1,16 +1,31 @@
 package foldingx.parser.func
 
 import foldingx.parser.FoldingParser
+import foldingx.parser.identifier.processId
 
 
 fun processNativeDef(defContext: FoldingParser.DefContext) =
     defContext.findJustDef()?.let { processJustDef(it)  to defContext.findInverseDefining().map { processInverseDef(it) } }
 
 fun processJustDef(justDefContext: FoldingParser.JustDefContext): CommonJustDef =
-    TODO("Not yet implemented")
+    CommonJustDef(
+        annotationBlockContext = justDefContext.findAnnotationBlock(),
+        id = justDefContext.findCommonIdentifier()!!.let(::processId),
+        typeParamContext = justDefContext.findTypeParam(),
+        parameterContext = justDefContext.findParameter(),
+        typeExContext = justDefContext.findTypeEx(),
+        valueContext = justDefContext.findValue()
+    )
 
 fun processInverseDef(inverseDefiningContext: FoldingParser.InverseDefiningContext): CommonInverseDef =
     TODO("Not yet implemented")
 
 fun processForeignDef(foreignDefContext: FoldingParser.ForeignDefContext): CommonForeignDef =
-    TODO("Not yet implemented")
+    CommonForeignDef(
+        annotationBlockContext = foreignDefContext.findAnnotationBlock(),
+        id = foreignDefContext.findCommonIdentifier()!!.let(::processId),
+        typeParamContext = foreignDefContext.findTypeParam(),
+        parameterContext = foreignDefContext.findParameter(),
+        typeExContext = foreignDefContext.findTypeEx(),
+        foreignBodyContext = foreignDefContext.findForeignBody()
+    )
