1422867599f8d68544aeeef4dc4fdb8b12eae40d
muqhc <muqhc07@gmail.com>
2022-12-11 21:04:36 +0900

    rename parameter

--- light-transpiler/light-transpiler-common/src/commonMain/kotlin/foldingx.lighttranspiler/LightDefTranspiler.kt
+++ light-transpiler/light-transpiler-common/src/commonMain/kotlin/foldingx.lighttranspiler/LightDefTranspiler.kt
@@ -8,9 +8,9 @@ import foldingx.parser.func.CommonJustDef
 interface LightDefTranspiler : LightValueTranspiler {
     fun transpileDef(fdDefContext: FoldingParser.DefContext): String
 
-    fun processJustDef(fdJustDefContext: CommonJustDef): String
-    fun processInverseDefining(fdInverseDefiningContext: CommonInverseDef): String
-    fun processForeignDef(fdForeignDefContext: CommonForeignDef): String
+    fun processJustDef(fdCommonJustDef: CommonJustDef): String
+    fun processInverseDefining(fdCommonInverseDef: CommonInverseDef): String
+    fun processForeignDef(fdCommonForeignDef: CommonForeignDef): String
 
     fun processParameter(fdParameterContext: FoldingParser.ParameterContext): String
     fun processParameterFromValue(fdParameterFromValueContext: FoldingParser.ParameterFromValueContext): String
