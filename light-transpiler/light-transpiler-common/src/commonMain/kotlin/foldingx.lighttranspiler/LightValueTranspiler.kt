435d9578cb8fc0e2d1e309e1a9aa2e4d2cfc0996
muqhc <muqhc07@gmail.com>
2022-12-16 23:56:08 +0900

    fix common interface

--- light-transpiler/light-transpiler-common/src/commonMain/kotlin/foldingx.lighttranspiler/LightValueTranspiler.kt
+++ light-transpiler/light-transpiler-common/src/commonMain/kotlin/foldingx.lighttranspiler/LightValueTranspiler.kt
@@ -5,16 +5,18 @@ import foldingx.parser.FoldingParser
 interface LightValueTranspiler {
     fun processValue(fdValueContext: FoldingParser.ValueContext): String
 
-    fun processJustDefaultValue(fdDefaultValueContext: FoldingParser.JustDefaultValueContext): String
+    fun processJustDefaultValue(fdJustDefaultValueContext: FoldingParser.JustDefaultValueContext): String
     fun processReflected(fdReflectedContext: FoldingParser.ReflectedContext): String
     fun processCallFunction(fdCallFunctionContext: FoldingParser.CallFunctionContext): String
+    fun processUseForeignClass(fdUseForeignClassContext: FoldingParser.UseForeignClassContext): String
     fun processGetFieldGlobal(fdGetFieldGlobalContext: FoldingParser.GetFieldGlobalContext): String
     fun processGetField(fdGetFieldContext: FoldingParser.GetFieldContext): String
     fun processCallMethod(fdCallMethodContext: FoldingParser.CallMethodContext): String
     fun processReflectedMethod(fdReflectedMethodContext: FoldingParser.ReflectedMethodContext): String
     fun processInvokeValue(fdInvokeValueContext: FoldingParser.InvokeValueContext): String
-    fun processIfExpression(fdIfExpressionContext: FoldingParser.IfExpressionContext): String
+    fun processSimpleIf(fdSimpleIfContext: FoldingParser.SimpleIfContext): String
     fun processTakeNull(fdTakeNullContext: FoldingParser.TakeNullContext): String
+    fun processIfExpression(fdIfExpressionContext: FoldingParser.IfExpressionContext): String
     fun processValueTypeCasting(fdValueTypeCastingContext: FoldingParser.ValueTypeCastingContext): String
     fun processCallAopFunc(fdCallAopFuncContext: FoldingParser.CallAopFuncContext): String
     fun processCallOpFunc(fdCallOpFuncContext: FoldingParser.CallOpFuncContext): String
