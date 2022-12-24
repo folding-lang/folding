ad2a7131707468168aa831b4c66cc8f7c510fc91
muqhc <muqhc07@gmail.com>
2022-12-24 15:22:51 +0900

    fix typing

--- light-transpiler/light-transpiler-common/src/commonMain/kotlin/foldingx.lighttranspiler/LightClassTranspiler.kt
+++ light-transpiler/light-transpiler-common/src/commonMain/kotlin/foldingx.lighttranspiler/LightClassTranspiler.kt
@@ -5,9 +5,9 @@ import foldingx.parser.FoldingParser
 interface LightClassTranspiler : LightDefTranspiler {
     fun transpileClass(fdClass_Context: FoldingParser.Class_Context): String
 
-    fun processJustClass(fdJustClassContext: FoldingParser.JustClassContext)
-    fun processJustMultiClass(fdJustMultiClassContext: FoldingParser.JustMultiClassContext)
-    fun processJustInterface(fdJustInterfaceContext: FoldingParser.JustInterfaceContext)
+    fun processJustClass(fdJustClassContext: FoldingParser.JustClassContext): String
+    fun processJustMultiClass(fdJustMultiClassContext: FoldingParser.JustMultiClassContext): String
+    fun processJustInterface(fdJustInterfaceContext: FoldingParser.JustInterfaceContext): String
 
     fun processDefInInterface(fdDefInInterfaceContext: FoldingParser.DefInInterfaceContext): String
     fun processDefInImpl(fdDefInImplContext: FoldingParser.DefInImplContext): String
