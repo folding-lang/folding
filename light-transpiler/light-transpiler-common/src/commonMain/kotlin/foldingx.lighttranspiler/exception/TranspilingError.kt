b94133fc1f494c65edc02684ccb36ff6e55b3da2
muqhc <muqhc07@gmail.com>
2022-12-25 00:00:56 +0900

    fix for abstract class constructor to be optional

--- light-transpiler/light-transpiler-common/src/commonMain/kotlin/foldingx.lighttranspiler/exception/TranspilingError.kt
+++ light-transpiler/light-transpiler-common/src/commonMain/kotlin/foldingx.lighttranspiler/exception/TranspilingError.kt
@@ -4,8 +4,9 @@ import org.antlr.v4.kotlinruntime.ParserRuleContext
 
 class TranspilingError(message: String) : Exception(message)
 
-fun ParserRuleContext.getPositionText() =
+fun ParserRuleContext.getPositionText() = try {
     position!!.start.let { (l,c) -> "$l:$c" }
+} catch (e: Exception) { "" }
 
 fun invalidCode(kind: String, context: ParserRuleContext) =
     TranspilingError(
