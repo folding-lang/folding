959e884a016168404e79aa372e957591ecd8cbd5
muqhc <muqhc07@gmail.com>
2022-12-24 15:21:45 +0900

    change class syntax structure

--- light-transpiler/light-transpiler-common/src/commonMain/kotlin/foldingx.lighttranspiler/LightClassTranspiler.kt
+++ light-transpiler/light-transpiler-common/src/commonMain/kotlin/foldingx.lighttranspiler/LightClassTranspiler.kt
@@ -5,8 +5,9 @@ import foldingx.parser.FoldingParser
 interface LightClassTranspiler : LightDefTranspiler {
     fun transpileClass(fdClass_Context: FoldingParser.Class_Context): String
 
-    fun processJustClass(fdJustClassContext: FoldingParser.JustClassContext): String
-    fun processJustInterface(fdJustInterfaceContext: FoldingParser.JustInterfaceContext): String
+    fun processJustClass(fdJustClassContext: FoldingParser.JustClassContext)
+    fun processJustMultiClass(fdJustMultiClassContext: FoldingParser.JustMultiClassContext)
+    fun processJustInterface(fdJustInterfaceContext: FoldingParser.JustInterfaceContext)
 
     fun processDefInInterface(fdDefInInterfaceContext: FoldingParser.DefInInterfaceContext): String
     fun processDefInImpl(fdDefInImplContext: FoldingParser.DefInImplContext): String
