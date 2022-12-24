2c66eac1313f1cef936ae0f02d021185926f7c68
muqhc <muqhc07@gmail.com>
2022-12-24 18:00:15 +0900

    implement transpileClass selecting

--- light-transpiler/light-transpiler-kotlin/src/commonMain/kotlin/foldingx.lighttranspiler/kotlin/LightClassTranspilerKt.kt
+++ light-transpiler/light-transpiler-kotlin/src/commonMain/kotlin/foldingx.lighttranspiler/kotlin/LightClassTranspilerKt.kt
@@ -4,7 +4,12 @@ import foldingx.lighttranspiler.LightClassTranspiler
 import foldingx.parser.FoldingParser
 
 interface LightClassTranspilerKt : LightClassTranspiler, LightDefTranspilerKt {
-    override fun transpileClass(fdClass_Context: FoldingParser.Class_Context): String
+    override fun transpileClass(fdClass_Context: FoldingParser.Class_Context): String = when(fdClass_Context) {
+        is FoldingParser.JustClassContext -> processJustClass(fdClass_Context)
+        is FoldingParser.JustMultiClassContext -> processJustMultiClass(fdClass_Context)
+        is FoldingParser.JustInterfaceContext -> processJustInterface(fdClass_Context)
+        else -> throw RuntimeException("Invalid class '${fdClass_Context.text}'")
+    }
 
     override fun processJustClass(fdJustClassContext: FoldingParser.JustClassContext): String
     override fun processJustMultiClass(fdJustMultiClassContext: FoldingParser.JustMultiClassContext): String
