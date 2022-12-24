c1d1ce3329e5af99392742396a0202a447202318
muqhc <muqhc07@gmail.com>
2022-12-24 21:55:30 +0900

    update interface

--- light-transpiler/light-transpiler-common/src/commonMain/kotlin/foldingx.lighttranspiler/LightClassTranspiler.kt
+++ light-transpiler/light-transpiler-common/src/commonMain/kotlin/foldingx.lighttranspiler/LightClassTranspiler.kt
@@ -6,9 +6,9 @@ interface LightClassTranspiler : LightDefTranspiler {
     fun transpileClass(fdClass_Context: FoldingParser.Class_Context): String
 
     fun processJustClass(fdJustClassContext: FoldingParser.JustClassContext): String
-    fun processJustMultiClass(fdJustMultiClassContext: FoldingParser.JustMultiClassContext): String
     fun processJustInterface(fdJustInterfaceContext: FoldingParser.JustInterfaceContext): String
+    fun processJustAbstractClass(fdJustAbstractClassContext: FoldingParser.JustAbstractClassContext): String
+    fun processJustMultiClass(fdJustMultiClassContext: FoldingParser.JustMultiClassContext): String
 
     fun processDefInInterface(fdDefInInterfaceContext: FoldingParser.DefInInterfaceContext): String
-    fun processDefInImpl(fdDefInImplContext: FoldingParser.DefInImplContext): String
 }
