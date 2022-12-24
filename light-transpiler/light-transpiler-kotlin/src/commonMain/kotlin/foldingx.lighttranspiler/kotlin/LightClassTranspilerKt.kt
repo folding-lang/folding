123ffd0e9def00b8b5902f8447165eb757f8ffd1
muqhc <muqhc07@gmail.com>
2022-12-24 15:24:00 +0900

    apply changed interface implementation

--- light-transpiler/light-transpiler-kotlin/src/commonMain/kotlin/foldingx.lighttranspiler/kotlin/LightClassTranspilerKt.kt
+++ light-transpiler/light-transpiler-kotlin/src/commonMain/kotlin/foldingx.lighttranspiler/kotlin/LightClassTranspilerKt.kt
@@ -7,6 +7,7 @@ interface LightClassTranspilerKt : LightClassTranspiler, LightDefTranspilerKt {
     override fun transpileClass(fdClass_Context: FoldingParser.Class_Context): String
 
     override fun processJustClass(fdJustClassContext: FoldingParser.JustClassContext): String
+    override fun processJustMultiClass(fdJustMultiClassContext: FoldingParser.JustMultiClassContext): String
     override fun processJustInterface(fdJustInterfaceContext: FoldingParser.JustInterfaceContext): String
 
     override fun processDefInInterface(fdDefInInterfaceContext: FoldingParser.DefInInterfaceContext): String
