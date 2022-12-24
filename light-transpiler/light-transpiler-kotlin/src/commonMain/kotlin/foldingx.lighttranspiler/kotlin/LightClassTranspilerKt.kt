51f1d1c815a06eec6c2bbf2bbf12212d7a4972a8
muqhc <muqhc07@gmail.com>
2022-12-24 23:36:18 +0900

    fix transpiling for aesthetic reasons

--- light-transpiler/light-transpiler-kotlin/src/commonMain/kotlin/foldingx.lighttranspiler/kotlin/LightClassTranspilerKt.kt
+++ light-transpiler/light-transpiler-kotlin/src/commonMain/kotlin/foldingx.lighttranspiler/kotlin/LightClassTranspilerKt.kt
@@ -2,6 +2,8 @@ package foldingx.lighttranspiler.kotlin
 
 import foldingx.lighttranspiler.LightClassTranspiler
 import foldingx.parser.FoldingParser
+import foldingx.parser.func.CommonJustDef
+import foldingx.parser.identifier.processId
 
 interface LightClassTranspilerKt : LightClassTranspiler, LightDefTranspilerKt {
     override fun transpileClass(fdClass_Context: FoldingParser.Class_Context): String = when(fdClass_Context) {
@@ -14,12 +16,12 @@ interface LightClassTranspilerKt : LightClassTranspiler, LightDefTranspilerKt {
     override fun processJustClass(fdJustClassContext: FoldingParser.JustClassContext): String {
         val (tHead,tTail) = fdJustClassContext.findTypeParam()?.let { processTypeParam(it).let { (h,t) ->
             " $h " to t
-        } } ?: (" " to "")
+        } } ?: ("" to "")
 
         val constructor = fdJustClassContext.findConstructorSelf()!!.findParameter()?.let { processParameter(it) } ?: "()"
         val initialize = fdJustClassContext.findConstructorSelf()!!.findDoBlock()?.let {
-            "init" + processDoBlock(it).removeSuffix("()")
-        }
+            "\ninit " + processDoBlock(it).removeSuffix("()") + "\n"
+        }?.insertMargin(4) ?: ""
 
         val interfaceList = fdJustClassContext.findImpl().map { processTypeEx(it.findTypeEx()!!) }
         val inherits = fdJustClassContext.findInherit()?.findImpl()?.findTypeEx()?.let { processTypeEx(it) }?.let {
@@ -33,7 +35,7 @@ interface LightClassTranspilerKt : LightClassTranspiler, LightDefTranspilerKt {
         val defListText = (vanillaDefList + implList).joinToString("\n\n","\n").insertMargin(4)
 
         val primaryHead = "class ${fdJustClassContext.ID()!!.text}$tHead$constructor $inheritsText $tTail"
-        val primaryBody = "{$defListText\n}"
+        val primaryBody = "{$initialize$defListText\n}"
 
         return primaryHead + primaryBody
     }
@@ -41,5 +43,23 @@ interface LightClassTranspilerKt : LightClassTranspiler, LightDefTranspilerKt {
     override fun processJustAbstractClass(fdJustAbstractClassContext: FoldingParser.JustAbstractClassContext): String
     override fun processJustInterface(fdJustInterfaceContext: FoldingParser.JustInterfaceContext): String
 
-    override fun processDefInInterface(fdDefInInterfaceContext: FoldingParser.DefInInterfaceContext): String
+    override fun processDefInInterface(fdDefInInterfaceContext: FoldingParser.DefInInterfaceContext): String {
+        val fdCommonJustDef = CommonJustDef(
+            fdDefInInterfaceContext.findAnnotationBlock(),
+            processId(fdDefInInterfaceContext.findCommonIdentifier()!!),
+            fdDefInInterfaceContext.findTypeParam(),
+            fdDefInInterfaceContext.findParameter(),
+            fdDefInInterfaceContext.findTypeEx(),
+            null
+        )
+        val (tHead, tTail) = fdCommonJustDef.typeParamContext?.let {
+            processTypeParam(it).let { (h, t) ->
+                " $h " to t
+            }
+        } ?: (" " to "")
+        val param = fdCommonJustDef.parameterContext?.let { p -> processParameter(p) } ?: "()"
+
+        return "fun$tHead${fdCommonJustDef.id}$param: ${processTypeEx(fdCommonJustDef.typeExContext!!)} " +
+                (tTail ?: "")
+    }
 }
