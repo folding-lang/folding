7da64f135038a6484ade53ca22ff23d8d199acdf
muqhc <muqhc07@gmail.com>
2022-12-25 16:50:15 +0900

    fix for gen beauty

--- light-transpiler/light-transpiler-kotlin/src/commonMain/kotlin/foldingx.lighttranspiler/kotlin/processTypeEx.kt
+++ light-transpiler/light-transpiler-kotlin/src/commonMain/kotlin/foldingx.lighttranspiler/kotlin/processTypeEx.kt
@@ -2,11 +2,11 @@ package foldingx.lighttranspiler.kotlin
 
 import foldingx.parser.FoldingParser
 
-fun processTypeEx(fdTypeExContext: FoldingParser.TypeExContext): String = "(" + when {
+fun processTypeEx(fdTypeExContext: FoldingParser.TypeExContext): String = when {
     fdTypeExContext.findTypeExSingle() != null -> processTypeExSingle(fdTypeExContext.findTypeExSingle()!!)
     fdTypeExContext.findTypeExFunc() != null -> processTypeExFunc(fdTypeExContext.findTypeExFunc()!!)
     else -> throw RuntimeException("Invalid typeEx '${fdTypeExContext.text}'")
-} + ")" + (fdTypeExContext.QM()?.let { "?" } ?: "")
+}.let { fdTypeExContext.QM()?.let { _ -> "($it)?" } ?: it }
 fun processTypeExSingle(fdTypeExSingleContext: FoldingParser.TypeExSingleContext): String =
     if (fdTypeExSingleContext.findPrimitiveType() == null)
         (fdTypeExSingleContext.findPackage_()?.text?.let { "$it." } ?: "") +
