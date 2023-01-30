03d1396ab4fdee4ebeea6798f4ee20d63fc4ad04
muqhc <muqhc07@gmail.com>
2023-01-30 23:39:28 +0900

    change grammar anonymousClassObject "[...]" -> "new {...}"

--- light-transpiler/light-transpiler-kotlin/src/jvmTest/kotlin/main.kt
+++ light-transpiler/light-transpiler-kotlin/src/jvmTest/kotlin/main.kt
@@ -12,8 +12,8 @@ fun main() {
     }
 //    val rawLexer = FoldingLexer(ANTLRFileStream.invoke("folding-stdlib/src/main/folding/folding/Number.fd"))
 //    val rawParser = FoldingParser(CommonTokenStream(rawLexer))
-    val sourceRoot = "folding-stdlib/src/main/folding"
-    val fileContextList = File("$sourceRoot/folding")
+    val sourceRoot = "samples"
+    val fileContextList = File(sourceRoot)
         .listFiles { _, name -> name.endsWith(".fd") }!!
         .map {
             FoldingParser(CommonTokenStream(FoldingLexer(CharStreams.fromFileName(it.path)))).file()
@@ -21,7 +21,7 @@ fun main() {
 
     val fileWrapperList = transpiler.transpilePackage(sourceRoot,fileContextList)
 
-    val outputSourceRoot = "folding-stdlib/src/main/kotlin"
+    val outputSourceRoot = "$sourceRoot/kotlin"
     val outputFileList = fileWrapperList.map {
         File(it.dirText.replace(sourceRoot,outputSourceRoot)).mkdirs()
         File(it.dirText.replace(sourceRoot,outputSourceRoot) + "/${it.name}").apply {
