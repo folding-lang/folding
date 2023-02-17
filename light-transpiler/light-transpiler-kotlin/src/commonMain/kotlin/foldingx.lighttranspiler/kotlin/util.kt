ff163581b8df9012e16f4b41c0322ae78efdf531
muqhc <muqhc07@gmail.com>
2023-02-17 15:08:34 +0900

    add mate param inv

--- light-transpiler/light-transpiler-kotlin/src/commonMain/kotlin/foldingx.lighttranspiler/kotlin/util.kt
+++ light-transpiler/light-transpiler-kotlin/src/commonMain/kotlin/foldingx.lighttranspiler/kotlin/util.kt
@@ -1,5 +1,12 @@
 package foldingx.lighttranspiler.kotlin
 
+import foldingx.parser.FoldingParser
+
 fun String.insertMargin(length: Int) = replace("\n","\n"+" ".repeat(length))
+
+fun mateParamAndParamCExes(parameterExes: List<FoldingParser.ParamExContext>, paramCExesText: String ): String =
+    parameterExes.zip(paramCExesText.split("\n")).joinToString("\n") { (it,invValue) ->
+        "val ${it.ID()}: ${processTypeEx(it.findTypeEx()!!)} = $invValue"
+    }
 
 
