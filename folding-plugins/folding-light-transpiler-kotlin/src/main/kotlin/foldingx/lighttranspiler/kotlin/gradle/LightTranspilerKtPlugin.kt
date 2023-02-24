8e03ecc8ada1c88fbbbc3cfa0e71cb0c409cabe6
muqhc <muqhc07@gmail.com>
2023-02-24 16:16:31 +0900

    fix logic

--- folding-plugins/folding-light-transpiler-kotlin/src/main/kotlin/foldingx/lighttranspiler/kotlin/gradle/LightTranspilerKtPlugin.kt
+++ folding-plugins/folding-light-transpiler-kotlin/src/main/kotlin/foldingx/lighttranspiler/kotlin/gradle/LightTranspilerKtPlugin.kt
@@ -51,12 +51,8 @@ open class LightTranspilerKtPlugin : Plugin<Project> {
             val task = target.tasks
                 .filterIsInstance<LightTranspileFoldingToKotlinTask>()
                 .find { it.name == name }
-            if (task != null)
-                target.tasks.create(name,LightTranspileFoldingToKotlinTask::class.java,it.outputPath,it.sourceSets)
-            else {
-                target.tasks.removeIf { it.name == name }
-                target.tasks.create(name,LightTranspileFoldingToKotlinTask::class.java,it.outputPath,it.sourceSets)
-            }
+            if (task != null) target.tasks.remove(task) // FIXME: tasks container does not support removing
+            target.tasks.create(name,LightTranspileFoldingToKotlinTask::class.java,it.outputPath,it.sourceSets)
         }
     }
 }
