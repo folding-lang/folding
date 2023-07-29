708c74508b5af5dc8fc1b2b42831794d61ac13d8
muqhc <muqhc07@gmail.com>
2023-07-29 14:18:03 +0900

    some fix

--- folding-plugins/folding-base/src/main/kotlin/foldingx/gradle/base/FoldingSourceSet.kt
+++ folding-plugins/folding-base/src/main/kotlin/foldingx/gradle/base/FoldingSourceSet.kt
@@ -5,6 +5,6 @@ import org.gradle.api.Named
 open class FoldingSourceSet(private val name: String): Named {
     var target: String? = null
     var sourceDir = "src/$name"
-    var outputDirs = mutableListOf("src/transpiled_fd/${name}")
+    var outputDirs = mutableListOf("src/${name}_transpiled_fd")
     override fun getName(): String = name
 }
