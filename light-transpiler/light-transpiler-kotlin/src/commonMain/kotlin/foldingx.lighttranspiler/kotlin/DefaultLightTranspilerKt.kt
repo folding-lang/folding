a15bb1d9bc559b7774d2e5d09146e6aadbfe6a73
muqhc <muqhc07@gmail.com>
2023-08-26 23:15:38 +0900

    fix implementing in DefaultLightTranspilerKt

--- light-transpiler/light-transpiler-kotlin/src/commonMain/kotlin/foldingx.lighttranspiler/kotlin/DefaultLightTranspilerKt.kt
+++ light-transpiler/light-transpiler-kotlin/src/commonMain/kotlin/foldingx.lighttranspiler/kotlin/DefaultLightTranspilerKt.kt
@@ -1,7 +1,10 @@
 package foldingx.lighttranspiler.kotlin
 
+import foldingx.parser.classes.CommonClass
+
 open class DefaultLightTranspilerKt: LightTranspilerKt {
     override var currentPackage: String? = null
+    override var generatedClassRegistry: MutableList<CommonClass>? = mutableListOf()
 
     companion object: DefaultLightTranspilerKt()
 }
